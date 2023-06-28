a798804f897b700733061abc5b7b41baf17f200f
Dejan Zivanov <punisher0504@gmail.com>
2023-06-28 08:36:16 +0200

    fixing insurance purchase error handling

--- resources/js/components/InsurancePurchaseComponent.vue
+++ resources/js/components/InsurancePurchaseComponent.vue
@@ -6,25 +6,29 @@
                 <div class="card h-100 mb-3 m-3 p-3">
                     <div class="form-group">
                         <label for="name">Name:</label>
-                        <input type="text" class="form-control" id="name" v-model="name">
+                        <input type="text" v-on:keydown="resetNameErrors" class="form-control" id="name" v-model="name">
+                        <div v-for="error in nameErrors" :key="error"><span class="text-red">{{ error }}</span></div>
                     </div>
                     <div class="form-group">
                         <label for="last-name">Last Name:</label>
-                        <input type="text" class="form-control" id="last-name" v-model="lastName">
+                        <input type="text " v-on:keydown="resetLastNameErrors"  class="form-control" id="last-name" v-model="lastName">
+                        <div v-for="error in lastNameErrors" :key="error"><span class="text-red">{{ error }}</span></div>
                     </div>
                     <div class="form-group">
                         <label for="holiday-date">Holiday Duration:</label>
 
-                        <date-picker class="form-control w-100" v-model="holidayDate" range format="DD.MM.YYYY." type="date" id="holiday-date"></date-picker>
+                        <date-picker :disabled-date="disabledDate" class="form-control w-100"  @change="resetDateErrors" v-model="holidayDate" range format="DD.MM.YYYY." type="date" id="holiday-date"></date-picker>
                     </div>
                     <div class="form-group">
                         <label for="number-of-days">Number of Days:</label>
 <!--                        <p>{{ holidayDate[1] }}  {{ holidayDate[0] }}</p>-->
-                        <p name="number-of-days" class="form-control h-50">{{ numberOfDays }}</p>
+                        <p style="min-height: 40px" name="number-of-days pt-2" class="form-control h-50 mb-0 pt-0 ">{{ numberOfDays }}</p>
+                        <div v-for="error in dateErrors" :key="error"><span class="text-red pt-2 mt-2">{{ error }}</span></div>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mt-3">
                         <label for="phone-number">Phone Number:</label>
-                        <input type="text" class="form-control" id="phone-number" v-model="phoneNumber">
+                        <input type="text" class="form-control" v-on:keydown="resetPhoneNumberErrors" id="phone-number" v-model="phoneNumber">
+                        <div v-for="error in phoneNumberErrors" :key="error"><span class="text-red">{{ error }}</span></div>
                     </div>
                     <div class="form-group">
                         <label for="policy-type">Policy Type:</label>
@@ -36,23 +40,26 @@
                 </div>
             </div>
             <div class="row" v-if="policyType === 'group'">
-                <div v-for="(member, index) in groupMembers" :key="index" class="col-md-4 pt-3 pb-3 m3-3">
+                <div :id="index" v-for="(member, index) in groupMembers" :key="index" class="col-md-4 pt-3 pb-3 m3-3">
                     <div class="card h-100 mb-3 m-3 p-3">
                         <h5>Group Member {{ index + 1 }}</h5>
                         <div class="form-group">
                             <label :for="'member-name-' + index">Name:</label>
-                            <input type="text" class="form-control" :id="'member-name-' + index" v-model="member.name">
+                            <input type="text" class="form-control" v-on:keydown="resetDependentsNameErrors" :id="'member-name-' + index" v-model="member.name">
+                            <div v-for="error in dependentNameErrors" :key="error"><span class="text-red">{{ error }}</span></div>
                         </div>
                         <div class="form-group pt-3">
                             <label :for="'member-last-name-' + index">Last Name:</label>
-                            <input type="text" class="form-control" :id="'member-last-name-' + index" v-model="member.lastName">
+                            <input type="text" class="form-control" v-on:keydown="resetDependentsLastNameErrors" :id="'member-last-name-' + index" v-model="member.lastName">
+                            <div v-for="error in dependentLastNameErrors" :key="error"><span class="text-red">{{ error }}</span></div>
                         </div>
-                        <div class="form-group pt-3">
+                        <div class="form-group pt-3" :id="'dependent-birth-date-' + index">
                             <label :for="'member-date-birth-' + index">Date Of Birth :</label>
-                                <date-picker  class="form-control" v-model="member.dateOfBirth" type="date" format="DD.MM.YYYY."></date-picker>
+                                <date-picker  id="dependent-date-picker" class="form-control" v-model="member.dateOfBirth" type="date" format="DD.MM.YYYY."></date-picker>
+                            <div v-for="error in dependentDateErrors" :key="error"><span class="text-red">{{ error }}</span></div>
                         </div>
                         <button type="button" class="btn btn-secondary mt-3" @click="addMember" v-if="index === groupMembers.length - 1 && isLastMemberValid">Add Member</button>
-                        <button type="button" class="btn btn-danger mt-3" @click="removeMember(index)" v-if="index === groupMembers.length - 1 && groupMembers.length > 1">Remove Member</button>
+                        <button type="button" class="btn btn-danger mt-3" @click="removeMember(index)" >Remove Member</button>
 
                     </div>
 
@@ -61,7 +68,8 @@
         </div>
         <div class="row mt-5 pt-5">
             <div class="d-flex justify-content-center align-items-center">
-                <button type="button" class="btn btn-success" @click="purchase" :disabled="isPurchaseDisabled">Purchase</button>
+<!--                <button type="button" class="btn btn-success" @click="purchase" :disabled="isPurchaseDisabled">Purchase</button>-->
+                <button type="button" class="btn btn-success" @click="purchase">Purchase</button>
             </div>
         </div>
     </div>
@@ -81,7 +89,19 @@ export default {
             policyType: 'individual',
             holidayDate: '',
             numberOfDays: '',
-            groupMembers: [{ name: '', lastName: '', dateOfBirth: '' }]
+            groupMembers: [{ name: '', lastName: '', dateOfBirth: '' }],
+            nameErrors: [],
+            lastNameErrors: [],
+            dateErrors: [],
+            phoneNumberErrors: [],
+            dependentNameErrors: [],
+            dependentLastNameErrors: [],
+            dependentDateErrors: [],
+
+
+
+
+
         };
     },
     watch: {
@@ -161,6 +181,16 @@ export default {
             if (index > 0) {
                 this.groupMembers.splice(index, 1);
             }
+            else if(index === 0 && this.groupMembers.length === 1)
+
+            {   this.policyType = 'individual';
+                this.groupMembers.splice(0, 1);
+                this.handlePolicyTypeChange();
+            }
+            else if(index === 0 && this.groupMembers.length > 1)
+            {
+                this.groupMembers.splice(index, 1);
+            }
         },
         handlePolicyTypeChange() {
             if (this.policyType === 'individual') {
@@ -179,6 +209,73 @@ export default {
         },
         purchase()
         {
+            if(this.name === '' || this.name === null || this.name === undefined)
+            {
+                if (!this.nameErrors.includes('Name is required')) {
+                    this.nameErrors.push('Name is required');
+                }
+                return;
+            }
+            if(this.lastName === '' || this.lastName === null || this.lastName === undefined)
+            {
+                if (!this.lastNameErrors.includes('Last Name is required')) {
+                    this.lastNameErrors.push('Last Name is required');
+                }
+                return;
+            }
+            if(this.holidayDate === '' || this.holidayDate === null || this.holidayDate === undefined)
+            {
+                if (!this.dateErrors.includes('Date is required')) {
+                    this.dateErrors.push('Date is required');
+                }
+                return;
+            }
+            if(this.phoneNumber === '' || this.phoneNumber === null || this.phoneNumber === undefined)
+            {
+                if (!this.phoneNumberErrors.includes('Phone Number is required')) {
+                    this.phoneNumberErrors.push('Phone Number is required');
+                }
+                return;
+            }
+
+            //ovde
+
+            const self = this;
+
+            for (let i = 0; i < this.groupMembers.length; i++) {
+                if(this.groupMembers[i].name === '' || this.groupMembers[i].name === null || this.groupMembers[i].name === undefined)
+                {
+                    let box = document.getElementById("member-name-" + i);
+
+                    if (!document.getElementById('error-message-name-' + i))
+                    {
+                        box.insertAdjacentHTML('afterend', '<div id="error-message-name-' + i + '"><span class="text-red">Name is required!</span></div>');
+                    }
+
+                    return;
+                }
+                if(this.groupMembers[i].lastName === '' || this.groupMembers[i].lastName === null || this.groupMembers[i].lastName === undefined)
+                {
+                    let box = document.getElementById("member-last-name-" + i);
+                    if (!document.getElementById('error-message-last-name-' + i)) {
+                        box.insertAdjacentHTML('afterend', '<div id="error-message-last-name-' + i + '"><span class="text-red">Last Name is required!3</span></div>');
+                    }
+                         return;
+                }
+                if(this.groupMembers[i].dateOfBirth === '' || this.groupMembers[i].dateOfBirth === null || this.groupMembers[i].dateOfBirth === undefined)
+                {
+                    if (!document.getElementById('error-message-birth-date-' + i)) {
+                        let box1 = document.getElementById("dependent-birth-date-" + i);
+                        box1.insertAdjacentHTML('beforeend', '<div id="error-message-birth-date-' + i + '"><span class="text-red">Date is required!3</span></div>');
+                    }
+                    return;
+                }
+            }
+
+
+
+
+
             const data = this.createDataObject();
 
             axios.post('/insurance-request', data)
@@ -228,6 +325,28 @@ export default {
                 icon: true,
                 rtl: false,
             });
+        },
+        disabledDate(date) {
+            const currentDate = new Date(); // Get the current date and time
+            return date.getTime() < currentDate.getTime(); // Disable dates before the current moment
+        },
+        resetNameErrors() {
+            this.nameErrors = [];
+        },
+        resetLastNameErrors() {
+            this.lastNameErrors = [];
+        },
+        resetPhoneNumberErrors() {
+            this.phoneNumberErrors = [];
+        },
+        resetDateErrors() {
+            this.dateErrors = [];
+        },
+        resetDependentsNameErrors() {
+            this.dependentNameErrors = [];
+        },
+        resetDependentsLastNameErrors() {
+            this.dependentLastNameErrors = [];
         },
     }
 };
